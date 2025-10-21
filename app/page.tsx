c80e63e951b02ed30ce81003a22a29f240a9fcf5
syedmehedi34 <syedmehedi34@gmail.com>
2025-10-22 01:08:17 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <div className="">
+      <h2>home</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+        necessitatibus rerum, sequi voluptatum deserunt ex quis sunt! Fugiat
+        odit, deleniti voluptates omnis eaque dolorem quaerat reprehenderit
+        ducimus voluptas harum ad eveniet sequi, blanditiis quia non fuga
+        voluptate necessitatibus expedita doloribus. Eius necessitatibus
+        asperiores perferendis voluptatibus dolore recusandae, laudantium ea
+        delectus.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
