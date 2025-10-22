73aabf30b519ca83aa8af957eb8e6f27492b03e2
syedmehedi34 <syedmehedi34@gmail.com>
2025-10-22 14:51:01 +0600

    header done fully

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -5,6 +5,7 @@ import { headerData } from "@/constants/data";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import SocialMedia from "./SocialMedia";
+import { useOutsideClick } from "@/hooks";
 interface SideBarProps {
   isOpen: boolean;
   onClose: () => void;
@@ -12,13 +13,18 @@ interface SideBarProps {
 
 const SideMenu: FC<SideBarProps> = ({ isOpen, onClose }) => {
   const pathName = usePathname();
+  const sideBarRef = useOutsideClick<HTMLDivElement>(onClose);
+
   return (
     <div
       className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white/70 shadow-xl ${
         isOpen ? "translate-x-0" : "-translate-x-full"
       } hoverEffect`}
     >
-      <div className="min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex flex-col gap-6">
+      <div
+        ref={sideBarRef}
+        className="min-w-72 max-w-96 bg-black h-screen p-10 borderr border-r-shop_light_green flex flex-col gap-6"
+      >
         <div className="flex items-center justify-between gap-5">
           <Logo className="text-white" spanDesign="group-hover:text-white" />
           <button
