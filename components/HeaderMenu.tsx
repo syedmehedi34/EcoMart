a33c8560214274d7acb58d32922df3361a33800b
syedmehedi34 <syedmehedi34@gmail.com>
2025-10-22 08:36:03 +0600

    header menu complete

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -2,6 +2,7 @@
 import { headerData } from "@/constants/data";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import path from "path";
 
 const HeaderMenu = () => {
   const pathName = usePathname();
@@ -12,14 +13,20 @@ const HeaderMenu = () => {
         <Link
           key={item?.title}
           href={item?.href}
-          className={`hover:text-shop_light_green hoverEffect relative group`}
+          className={`hover:text-shop_light_green hoverEffect relative group ${
+            pathName === item?.href && "text-shop_light_green"
+          }`}
         >
           {item?.title}
           <span
-            className={`absolute -bottom-0.5 left-1/2 w-0 h-0.5 bg-shop_light_green group-hover:w-1/2 hoverEffect group-hover:left-0`}
+            className={`absolute -bottom-0.5 left-1/2 w-0 h-0.5 bg-shop_light_green group-hover:w-1/2 hoverEffect group-hover:left-0 ${
+              pathName === item?.href && "w-1/2"
+            }`}
           />
           <span
-            className={`absolute -bottom-0.5 right-1/2 w-0 h-0.5 bg-shop_light_green group-hover:w-1/2 hoverEffect group-hover:right-0`}
+            className={`absolute -bottom-0.5 right-1/2 w-0 h-0.5 bg-shop_light_green group-hover:w-1/2 hoverEffect group-hover:right-0 ${
+              pathName === item?.href && "w-1/2"
+            }`}
           />
         </Link>
       ))}
