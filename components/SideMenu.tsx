80f14081c6f7e0c1c82d70a7f7c42ca8e3c63403
syedmehedi34 <syedmehedi34@gmail.com>
2025-10-22 11:44:56 +0600

    side menu added

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from "react";
 import Logo from "./Logo";
 import { X } from "lucide-react";
+import { headerData } from "@/constants/data";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 interface SideBarProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
 const SideMenu: FC<SideBarProps> = ({ isOpen, onClose }) => {
+  const pathName = usePathname();
   return (
     <div
-      className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white/80 shadow-xl ${
+      className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white/70 shadow-xl ${
         isOpen ? "translate-x-0" : "-translate-x-full"
       } hoverEffect`}
     >
@@ -23,7 +27,20 @@ const SideMenu: FC<SideBarProps> = ({ isOpen, onClose }) => {
             <X />
           </button>
         </div>
-        <div></div>
+
+        <div className="flex flex-col space-y-3.5 font-semibold tracking-wide">
+          {headerData.map((item) => (
+            <Link
+              href={item?.href}
+              key={item?.title}
+              className={`hover:text-shop_light_green hoverEffect ${
+                pathName === item?.href && "text-shop_light_green"
+              }`}
+            >
+              {item?.title}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
