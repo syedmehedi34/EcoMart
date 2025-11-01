2784801b0811f131e9b76c2200b61eef186944e8
syedmehedi34 <syedmehedi34@gmail.com>
2025-11-01 21:03:46 +0600

    paypal logo added

---
+++ images/index.ts
@@ -0,0 +1,6 @@
+import paypalLogo from "./paypalLogo.png";
+import emptyCart from "./emptyCart.png";
+import banner from "./banner/banner_1.png";
+
+// export all
+export { paypalLogo, emptyCart, banner };
