92c26b80e034bc9a9df1ec24916d1ccbc53bae21
syedmehedi34 <syedmehedi34@gmail.com>
2025-11-01 19:28:18 +0600

    added assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className="font-poppins antialiased">
+        <body className="font-poppins antialiased" suppressHydrationWarning>
           <Header />
 
           {children}
