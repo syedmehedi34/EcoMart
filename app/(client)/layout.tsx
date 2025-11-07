1bfcddf3dcf6b3a9c2f4520fba91e1e047bf35c5
syedmehedi34 <syedmehedi34@gmail.com>
2025-11-08 01:55:32 +0600

    sanity path added

---
+++ app/(client)/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { ClerkProvider } from "@clerk/nextjs";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - EcoMart",
+    default: "EcoMart Online Store",
+  },
+  description: "Your one-stop shop for eco-friendly products",
+};
+// suppressHydrationWarning
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      {/* <html lang="en" className="font-poppins">
+        <body suppressHydrationWarning> */}
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
+      {/* </body>
+      </html> */}
+    </ClerkProvider>
+  );
+}
+
+/*
+use:   sanity for data management  
+use:   next-sanity package for easy integration with Next.js
+npm:   npx sanity@latest init
+web link: https://www.npmjs.com/package/next-sanity
+
+*/
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,9 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { ClerkProvider } from "@clerk/nextjs";
-
-export const metadata: Metadata = {
-  title: {
-    template: "%s - EcoMart",
-    default: "EcoMart Online Store",
-  },
-  description: "Your one-stop shop for eco-friendly products",
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body className="font-poppins antialiased">{children}</body>
+    </html>
+  );
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <ClerkProvider>
-      <html lang="en" className="font-poppins">
-        <body className="font-poppins antialiased" suppressHydrationWarning>
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1">{children}</main>
-            <Footer />
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
-}
-
-/*
-use:   sanity for data management  
-use:   next-sanity package for easy integration with Next.js
-npm:   npx sanity@latest init
-web link: https://www.npmjs.com/package/next-sanity
-
-*/
+export default RootLayout;
