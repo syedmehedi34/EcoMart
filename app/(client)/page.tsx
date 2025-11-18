5a9ae21b09995e46272f1a1a9133e990b8462896
syedmehedi34 <syedmehedi34@gmail.com>
2025-11-18 23:49:48 +0600

    updated

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,10 +1,14 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
+import ProductGrid from "@/components/ProductGrid";
 
 const Home = () => {
   return (
-    <Container className="">
+    <Container className="bg-shop_light_pink">
       <HomeBanner />
+      <div>
+        <ProductGrid />
+      </div>
     </Container>
   );
 };
---
+++ components/ProductGrid.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductGrid = () => {
+  return <div>ProductGrid</div>;
+};
+
+export default ProductGrid;
